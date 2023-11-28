71304e30d21b5a60d869805be0fecfe24fdfc2cb
adazem009 <68537469+adazem009@users.noreply.github.com>
2023-11-28 18:23:07 +0100

    Init QCoreApplication in tests

--- test/common.h
+++ test/common.h
@@ -1,11 +1,13 @@
 #pragma once
 
+#include <QCoreApplication>
 #include <gtest/gtest.h>
 #include <filesystem>
 #include <fstream>
 
 int main(int argc, char **argv)
 {
+    QCoreApplication a(argc, argv);
     std::filesystem::current_path(DATA_DIR);
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
