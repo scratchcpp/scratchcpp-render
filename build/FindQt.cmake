52b14e85e88891dde5063c6182c80f63110ae88f
adazem009 <68537469+adazem009@users.noreply.github.com>
2023-11-28 09:17:02 +0100

    Add a variable for Qt modules

--- build/FindQt.cmake
+++ build/FindQt.cmake
@@ -1,1 +1,6 @@
 find_package(Qt6 6.6 COMPONENTS Gui Qml Quick REQUIRED)
+set(QT_LIBS Qt6::Gui Qt6::Qml Qt6::Quick)
+
+function(linkQt TARGET)
+    target_link_libraries(${TARGET} PRIVATE ${QT_LIBS})
+endfunction()
