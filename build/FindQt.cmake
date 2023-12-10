dbe3f549bebc493e1c6517afce46dccc7fa6c72c
adazem009 <68537469+adazem009@users.noreply.github.com>
2023-12-10 18:47:02 +0100

    Add Qt SVG module

--- build/FindQt.cmake
+++ build/FindQt.cmake
@@ -1,5 +1,5 @@
-find_package(Qt6 6.6 COMPONENTS Gui Qml Quick REQUIRED)
-set(QT_LIBS Qt6::Gui Qt6::Qml Qt6::Quick)
+find_package(Qt6 6.6 COMPONENTS Gui Qml Quick Svg REQUIRED)
+set(QT_LIBS Qt6::Gui Qt6::Qml Qt6::Quick Qt6::Svg)
 
 if (SCRATCHCPPGUI_BUILD_UNIT_TESTS)
     find_package(Qt6 6.6 COMPONENTS Test REQUIRED)
