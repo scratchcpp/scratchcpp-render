c6d92ed20066a137063c36b78a77dff9231603ce
adazem009 <68537469+adazem009@users.noreply.github.com>
2023-12-25 01:16:12 +0100

    Add SceneMouseArea class

---
+++ ScratchCPPGui/scenemousearea.h
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+#pragma once
+
+#include <QQuickItem>
+
+namespace scratchcppgui
+{
+
+class IRenderedTarget;
+class MouseEventHandler;
+
+class SceneMouseArea : public QQuickItem
+{
+        Q_OBJECT
+        QML_ELEMENT
+        Q_PROPERTY(IRenderedTarget *stage WRITE setStage NOTIFY stageChanged)
+        Q_PROPERTY(QQuickItem *spriteRepeater READ spriteRepeater WRITE setSpriteRepeater NOTIFY spriteRepeaterChanged)
+
+    public:
+        explicit SceneMouseArea(QQuickItem *parent = nullptr);
+
+        IRenderedTarget *stage() const;
+        void setStage(IRenderedTarget *newStage);
+
+        QQuickItem *spriteRepeater() const;
+        void setSpriteRepeater(QQuickItem *newSpriteRepeater);
+
+    signals:
+        void mouseMoved(qreal x, qreal y);
+        void mousePressed();
+        void mouseReleased();
+        void stageChanged();
+        void spriteRepeaterChanged();
+
+    private:
+        MouseEventHandler *m_mouseHandler = nullptr;
+};
+
+} // namespace scratchcppgui
