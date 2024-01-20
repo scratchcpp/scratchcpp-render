3db89da7d61ee06dae0614699c484fc7626c3075
adazem009 <68537469+adazem009@users.noreply.github.com>
2024-01-20 16:12:31 +0100

    Add BitmapSkin class

---
+++ src/bitmapskin.h
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+#pragma once
+
+#include "skin.h"
+#include "texture.h"
+
+namespace scratchcpprender
+{
+
+class BitmapSkin : public Skin
+{
+    public:
+        BitmapSkin(libscratchcpp::Costume *costume);
+        ~BitmapSkin();
+
+        Texture getTexture(double scale) const override;
+        double getTextureScale(const Texture &texture) const override;
+
+    protected:
+        void paint(QPainter *painter) override;
+
+    private:
+        Texture m_texture;
+        QSize m_textureSize;
+        QImage m_image;
+};
+
+} // namespace scratchcpprender
---
+++ test/skins/bitmapskin_test.cpp
@@ -0,0 +1,74 @@
+#include <scratchcpp/costume.h>
+#include <bitmapskin.h>
+
+#include "../common.h"
+
+using namespace scratchcpprender;
+using namespace libscratchcpp;
+
+class BitmapSkinTest : public testing::Test
+{
+    public:
+        void SetUp() override
+        {
+            m_context.create();
+            ASSERT_TRUE(m_context.isValid());
+
+            m_surface.setFormat(m_context.format());
+            m_surface.create();
+            Q_ASSERT(m_surface.isValid());
+            m_context.makeCurrent(&m_surface);
+
+            Costume jpegCostume("", "", "");
+            std::string costumeData = readFileStr("image.jpg");
+            jpegCostume.setData(costumeData.size(), costumeData.data());
+            m_jpegSkin = std::make_unique<BitmapSkin>(&jpegCostume);
+
+            Costume pngCostume("", "", "");
+            costumeData = readFileStr("image.png");
+            pngCostume.setData(costumeData.size(), costumeData.data());
+            m_pngSkin = std::make_unique<BitmapSkin>(&pngCostume);
+        }
+
+        void TearDown() override
+        {
+            ASSERT_EQ(m_context.surface(), &m_surface);
+            m_context.doneCurrent();
+        }
+
+        QOpenGLContext m_context;
+        QOffscreenSurface m_surface;
+        std::unique_ptr<Skin> m_jpegSkin;
+        std::unique_ptr<Skin> m_pngSkin;
+};
+
+TEST_F(BitmapSkinTest, GetTexture)
+{
+    Texture texture = m_jpegSkin->getTexture(1);
+    ASSERT_EQ(texture.width(), 4);
+    ASSERT_EQ(texture.height(), 6);
+
+    QBuffer jpegBuffer;
+    texture.toImage().save(&jpegBuffer, "png");
+    QFile jpegRef("jpeg_result.png");
+    jpegRef.open(QFile::ReadOnly);
+    jpegBuffer.open(QBuffer::ReadOnly);
+    ASSERT_EQ(jpegBuffer.readAll(), jpegRef.readAll());
+
+    texture = m_pngSkin->getTexture(1);
+    ASSERT_EQ(texture.width(), 4);
+    ASSERT_EQ(texture.height(), 6);
+
+    QBuffer pngBuffer;
+    texture.toImage().save(&pngBuffer, "png");
+    QFile pngRef("png_result.png");
+    pngRef.open(QFile::ReadOnly);
+    pngBuffer.open(QBuffer::ReadOnly);
+    ASSERT_EQ(pngBuffer.readAll(), pngRef.readAll());
+}
+
+TEST_F(BitmapSkinTest, GetTextureScale)
+{
+    ASSERT_EQ(m_jpegSkin->getTextureScale(Texture()), 1);
+    ASSERT_EQ(m_pngSkin->getTextureScale(Texture()), 1);
+}
