5818643fc9b176d860b6eceecf30858e65f550c1
adazem009 <68537469+adazem009@users.noreply.github.com>
2024-01-20 16:06:26 +0100

    Add Skin class

---
+++ src/skin.h
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+#pragma once
+
+#include <QPointF>
+#include <QSizeF>
+#include <QtOpenGL>
+
+namespace libscratchcpp
+{
+
+class Costume;
+
+}
+
+namespace scratchcpprender
+{
+
+class Texture;
+
+class Skin
+{
+    public:
+        Skin(libscratchcpp::Costume *costume);
+        Skin(const Skin &) = delete;
+        virtual ~Skin() { }
+
+        virtual Texture getTexture(double scale) const = 0;
+        virtual double getTextureScale(const Texture &texture) const = 0;
+
+    protected:
+        Texture createAndPaintTexture(int width, int height, bool multisampled);
+        virtual void paint(QPainter *painter) = 0;
+};
+
+} // namespace scratchcpprender
