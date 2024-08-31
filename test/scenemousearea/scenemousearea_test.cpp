3b639b29f495132a7fed206c6de0c821091c64fc
adazem009 <68537469+adazem009@users.noreply.github.com>
2024-08-31 13:50:56 +0200

    Add test for SceneMouseArea events
    Resolves: #93

--- test/scenemousearea/scenemousearea_test.cpp
+++ test/scenemousearea/scenemousearea_test.cpp
@@ -7,12 +7,64 @@
 
 using namespace scratchcpprender;
 
+using ::testing::NiceMock;
+
 TEST(SceneMouseAreaTest, Constructors)
 {
     SceneMouseArea area1;
     SceneMouseArea area2(&area1);
     ASSERT_EQ(area2.parent(), &area1);
     ASSERT_EQ(area2.parentItem(), &area1);
+}
+
+TEST(SceneMouseAreaTest, Events)
+{
+    QPointingDevice dev;
+    SceneMouseArea mouseArea;
+    ProjectLoader loader;
+    mouseArea.setProjectLoader(&loader);
+    NiceMock<RenderedTargetMock> stage;
+    mouseArea.setStage(&stage);
+
+    // mouseMoved
+    {
+        QHoverEvent event(QEvent::HoverMove, {}, {}, {}, Qt::NoModifier, &dev);
+        QSignalSpy spy(&mouseArea, &SceneMouseArea::mouseMoved);
+        QCoreApplication::sendEvent(&mouseArea, &event);
+        ASSERT_EQ(spy.count(), 1);
+    }
+
+    // mousePressed
+    {
+        QMouseEvent event(QEvent::MouseButtonPress, {}, {}, {}, Qt::LeftButton, Qt::LeftButton, Qt::NoModifier, &dev);
+        QSignalSpy spy(&mouseArea, &SceneMouseArea::mousePressed);
+        QCoreApplication::sendEvent(&mouseArea, &event);
+        ASSERT_EQ(spy.count(), 1);
+    }
+
+    // mouseReleased
+    {
+        QMouseEvent event(QEvent::MouseButtonRelease, {}, {}, {}, Qt::LeftButton, Qt::LeftButton, Qt::NoModifier, &dev);
+        QSignalSpy spy(&mouseArea, &SceneMouseArea::mouseReleased);
+        QCoreApplication::sendEvent(&mouseArea, &event);
+        ASSERT_EQ(spy.count(), 1);
+    }
+
+    // mouseWheelUp
+    {
+        QWheelEvent event(QPointF(), QPointF(), QPoint(2, 3), QPoint(10, 15), Qt::LeftButton, Qt::NoModifier, Qt::NoScrollPhase, false);
+        QSignalSpy spy(&mouseArea, &SceneMouseArea::mouseWheelUp);
+        QCoreApplication::sendEvent(&mouseArea, &event);
+        ASSERT_EQ(spy.count(), 1);
+    }
+
+    // mouseWheelDown
+    {
+        QWheelEvent event(QPointF(), QPointF(), QPoint(2, 3), QPoint(10, -15), Qt::LeftButton, Qt::NoModifier, Qt::NoScrollPhase, false);
+        QSignalSpy spy(&mouseArea, &SceneMouseArea::mouseWheelDown);
+        QCoreApplication::sendEvent(&mouseArea, &event);
+        ASSERT_EQ(spy.count(), 1);
+    }
 }
 
 TEST(SceneMouseAreaTest, Stage)
